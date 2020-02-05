98f2491995ebddd994023326014c338445884011
BrianLeber <59613693+BrianLeber@users.noreply.github.com>
2020-02-05 15:39:27 -0600

    End of Lesson - initial project compelte

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
-var newItem = document.getElementsByID("addItemButton");
+var itemList = document.querySelector("#my-List");
+var addButton = document.getElementById("addItemButton");
 var headline = document.getElementById("my-Headline");
 var listItems = document.getElementById("my-List").getElementsByTagName("li");
 var newText = "Hello World!";
 
-for (i = 0; i < listItems.length; i++) {
-  // listItems[i].innerHTML = newText;
-  listItems[i].addEventListener("click", activateItem);
+addButton.addEventListener("click", createNewItem);
+
+itemList.addEventListener("click", activateItem);
+//for (i = 0; i < listItems.length; i++) {
+// listItems[i].innerHTML = newText;
+//  listItems[i].addEventListener("click", activateItem);
+//}
+
+function activateItem(e) {
+  // alert("click detected");
+  if (e.target.nodeName == "LI") {
+    headline.innerHTML = e.target.innerHTML;
+    for (i = 0; i < e.target.parentNode.children.length; i++) {
+      // listItems[i].innerHTML = newText;
+      e.target.parentNode.children[i].classList.remove("active");
+    }
+
+    e.target.classList.add("active");
+  }
 }
 
-function activateItem() {
-  // alert("click detected");
-  headline.innerHTML = this.innerHTML;
+function createNewItem() {
+  itemList.innerHTML += "<li>something new " + listItems.length + "</li>";
 }
