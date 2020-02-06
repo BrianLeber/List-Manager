fc9576270d018fe0cf901e162fbb97bc60b108f5
BrianLeber <59613693+BrianLeber@users.noreply.github.com>
2020-02-06 13:01:48 -0600

    Added Update and Complete highligh toggling

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 var itemList = document.querySelector("#my-List");
 var addButton = document.getElementById("addItemButton");
+var removeButton = document.getElementById("removeItemButton");
+var updateButton = document.getElementById("updateItemButton");
+var deactivateButton = document.getElementById("deactivateItemButton");
 var headline = document.getElementById("my-Headline");
 var listItems = document.getElementById("my-List").getElementsByTagName("li");
 var newText = "Hello World!";
 
 addButton.addEventListener("click", createNewItem);
-
+removeButton.addEventListener("click", removeItem);
+updateButton.addEventListener("click", updateItem);
+deactivateButton.addEventListener("click", deactivateItem);
 itemList.addEventListener("click", activateItem);
 //for (i = 0; i < listItems.length; i++) {
 // listItems[i].innerHTML = newText;
@@ -15,16 +20,38 @@ itemList.addEventListener("click", activateItem);
 function activateItem(e) {
   // alert("click detected");
   if (e.target.nodeName == "LI") {
-    headline.innerHTML = e.target.innerHTML;
+    headline.value = e.target.innerHTML;
     for (i = 0; i < e.target.parentNode.children.length; i++) {
-      // listItems[i].innerHTML = newText;
       e.target.parentNode.children[i].classList.remove("active");
     }
-
     e.target.classList.add("active");
   }
 }
 
+function deactivateItem(e) {
+  e = document.querySelector("li.active.inactive");
+  if (e !== null) {
+    e.classList.remove("inactive");
+  } else {
+    e = document.querySelector("li.active");
+    e.classList.add("inactive");
+  }
+}
+
 function createNewItem() {
-  itemList.innerHTML += "<li>something new " + listItems.length + "</li>";
+  if (headline.value == "") {
+    alert("please enter some text");
+  } else {
+    itemList.innerHTML += "<li>" + headline.value + "</li>";
+  }
+}
+function removeItem(e) {
+  e = document.querySelector("li.active");
+  if (e !== null) {
+    e.parentElement.removeChild(e);
+  }
+}
+
+function updateItem() {
+  document.querySelector("li.active").innerHTML = headline.value;
 }
